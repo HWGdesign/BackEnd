37f00b8b59a8e62d90bbeaa152897cf2e787adb0
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-07-11 14:00:03 +0200

    Merged with MongoDB, added functionality

--- Practice/app.js
+++ Practice/app.js
@@ -1,33 +1,92 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-//express application
+//! express application
+
 const app = express();
 
+//! connect to mongodb
+
+const dbURI = 'mongodb+srv://randomUser:randomPassword@hwgcluster.aq1dgom.mongodb.net/hwg-blog';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
 //ejs engine
 app.set('view engine', 'ejs');
 
-//request
-app.listen(3000);
+//! middleware & static files
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// mongoose and mongo sandbox
+// app.get('/add-blog', (req, res) => {
+//   const blog = new Blog({ title: 'newBlog2', snippet: 'About', content: 'blaBlaBla' });
+//   blog
+//     .save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.get('/all-blogs', (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.get('/single-blog', (req, res) => {
+//   Blog.findById('64ad33960d0822c38515e1f9')
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+//! routes
 
 app.get('/', (req, res) => {
-  const blogs = [
-    { title: 'Title 1', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quibusdam!' },
-    { title: 'Title 2', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quibusdam!' },
-    { title: 'Title 3', snippet: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quibusdam!' },
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
+//! blog routes
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => console.log(err));
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('blogs/create', { title: 'Create' });
 });
 
-//404
+//! 404 - Error
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('404', { title: '404' });
 });
